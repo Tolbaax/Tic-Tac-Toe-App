5d0cd1df1d1d0671ee940c57c3879e3b9d38a6a4
Mohamed Tolba <Tolbaax@gmail.com>
2021-08-24 22:03:40 +0200

    Tic-Tac-Toe App

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(tictactoe());
+}
+class tictactoe extends StatefulWidget {
+  const tictactoe({Key? key}) : super(key: key);
+
+  @override
+  _tictactoeState createState() => _tictactoeState();
+}
+
+class _tictactoeState extends State<tictactoe> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+
+        ),
+      ),
+    );
+  }
+}
